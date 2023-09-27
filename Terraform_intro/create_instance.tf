04423efe3565171505b4d74ca29b7ba443bf0b77
Bolaji Sal <salabiubolaji@gmail.com>
2023-09-27 12:08:22 -0400

    ec2-intro

--- Terraform_intro/create_instance.tf
+++ Terraform_intro/create_instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
-  ami           = "ami-0f409bae3775dc8e5"
+  ami           = "ami-00c6177f250e07ec1"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstnce-${count.index}"
+    Name = "MyFirstInstnace-${count.index}"
   }
 }
