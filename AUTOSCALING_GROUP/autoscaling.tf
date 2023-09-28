1686ea37d3c3d4d03e38be0d57eb6e869f8048bb
Bolaji Sal <salabiubolaji@gmail.com>
2023-09-28 11:40:59 -0400

    update

--- AUTOSCALING_GROUP/autoscaling.tf
+++ AUTOSCALING_GROUP/autoscaling.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "realtechie-launchconfig" {
   name_prefix     = "realtechie-launchconfig"
   image_id        = lookup(var.AMIS, var.AWS_REGION)
   instance_type   = "t2.micro"
-  key_name        = aws_key_pair.levelup_key.key_name
+  key_name        = aws_key_pair.realtechie_key.key_name
 }
 
 #Generate Key
