063c9e4fa38b8a7c302330eb8b19b997e0e34f4c
Bolaji Sal <salabiubolaji@gmail.com>
2023-09-28 06:30:49 -0400

    commit ec2instance

---
+++ Network/create_ec2instance.tf
@@ -0,0 +1,21 @@
+resource "aws_key_pair" "realtechie_key" {
+    key_name = "realtechie_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.realtechie_key.key_name
+
+  vpc_security_group_ids = [aws_security_group.realtechie-sg.id]
+  subnet_id = aws_subnet.realtechiePub-Sub1.id
+
+  tags = {
+    Name = "MyFirstInstnace"
+  }
+
+}
+
+#TO CREATE SSH-KEYGEN
+#ssh-keygen -f <keyname>
