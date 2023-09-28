4047564bb890ac173c6936d1ca8542f5d753be1b
Bolaji Sal <salabiubolaji@gmail.com>
2023-09-28 07:45:15 -0400

    edited

--- USER_DATA/security_group.tf
+++ USER_DATA/security_group.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "realtechie-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
   tags = {
     Name = "realtechie-sg"
