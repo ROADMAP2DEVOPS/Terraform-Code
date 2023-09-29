10fc91ebb59ebd543d6e425a38cf717cb0c91584
Bolaji Sal <salabiubolaji@gmail.com>
2023-09-29 08:44:15 -0400

    wish commit

--- Terraform_Modules/environment/dev/backend.tf
+++ Terraform_Modules/environment/dev/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-    backend "s3-bucket" {
+    backend "s3" {
         bucket          = "my-terraformed-project"
         key             = "environments/dev/terraform_state"
         region          = var.AWS_REGION
