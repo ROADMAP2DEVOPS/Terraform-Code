4b3cfcb10d988ac224d8890afe7db9265370499e
Bolaji Sal <salabiubolaji@gmail.com>
2023-09-29 08:45:42 -0400

    provider set

--- Terraform_Modules/environment/dev/backend.tf
+++ Terraform_Modules/environment/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket          = "my-terraformed-project"
         key             = "environments/dev/terraform_state"
-        region          = var.AWS_REGION
+        region          = "us-east-1"
     }
 
 }
